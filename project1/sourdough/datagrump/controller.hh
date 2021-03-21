c1618172350eff177ebe8c0219ed73c5fb97cf95
Samuel Bedassa Alemu <samuelbe@student.ethz.ch>
2021-03-21 15:45:58 +0100

    Replace controller.hh

--- project1/sourdough/datagrump/controller.hh
+++ project1/sourdough/datagrump/controller.hh
@@ -11,7 +11,6 @@ private:
   bool debug_; /* Enables debugging output */
 
   /* Add member variables here */
-  unsigned int size;
 
 public:
   /* Public interface for the congestion controller */
