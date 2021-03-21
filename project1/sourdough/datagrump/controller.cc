3fa21c37aa869cbc3d522e86d8541acbff218a6f
Samuel Bedassa Alemu <samuelbe@student.ethz.ch>
2021-03-21 22:17:09 +0100

    Replace controller.cc

--- project1/sourdough/datagrump/controller.cc
+++ project1/sourdough/datagrump/controller.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), size(4), min(45), seq(1), slow_start(true)
+  : debug_( debug )//, size(4), min(45), seq(1), slow_start(true)
 {}
 
 /* Get current window size, in datagrams */
@@ -15,14 +15,14 @@ unsigned int Controller::window_size()
 {
   //42 or 45
   
-  
+  /*
   unsigned int the_window_size = size;
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
 	 << " window size is " << the_window_size << endl;
   }
-
-  return size;
+*/
+  return 45;
 }
 
 /* A datagram was sent */
@@ -33,12 +33,12 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 				    const bool after_timeout
 				    /* datagram was sent because of a timeout */ )
 {
-  /* Default: take no action */
+  /* Default: take no action 
   if(after_timeout){
     size /=2;
     slow_start = false;
   }
-  
+  */
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
 	 << " sent datagram " << sequence_number << " (timeout = " << after_timeout << ")\n";
@@ -55,8 +55,7 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
-  /* Default: take no action */
- 
+  /* Default: take no action 
     int n = recv_timestamp_acked - send_timestamp_acked; 
     int m = timestamp_ack_received - recv_timestamp_acked;
     uint64_t max_acked_seq = max(max_acked_seq, sequence_number_acked);
@@ -74,7 +73,8 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
     size = size-2 ;
   }
   
-
+*/
+ 
   /*
   
   
