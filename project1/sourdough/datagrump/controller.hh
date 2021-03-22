95d9d367588330754a5482f48c45900db624acff
Samuel Bedassa Alemu <samuelbe@student.ethz.ch>
2021-03-22 10:09:55 +0100

    Replace controller.hh

--- project1/sourdough/datagrump/controller.hh
+++ project1/sourdough/datagrump/controller.hh
@@ -1,46 +1,51 @@
-#ifndef CONTROLLER_HH
-#define CONTROLLER_HH
+#ifndef CONTEST_MESSAGE_HH
+#define CONTEST_MESSAGE_HH
 
+#include <string>
 #include <cstdint>
 
-/* Congestion controller interface */
-
-class Controller
+struct ContestMessage
 {
-private:
-  bool debug_; /* Enables debugging output */
+  struct Header {
+    uint64_t sequence_number;
+    uint64_t send_timestamp;
 
-  /* Add member variables here*/
-  unsigned int size;
-  unsigned int min;
-  unsigned int seq;
-  bool slow_start;
- 
-public:
-  /* Public interface for the congestion controller */
-  /* You can change these if you prefer, but will need to change
-     the call site as well (in sender.cc) */
+    uint64_t ack_sequence_number;
+    uint64_t ack_send_timestamp;
+    uint64_t ack_recv_timestamp;
+    uint64_t ack_payload_length;
 
-  /* Default constructor */
-  Controller( const bool debug );
+    /* Header for new message */
+    Header( const uint64_t s_sequence_number );
 
-  /* Get current window size, in datagrams */
-  unsigned int window_size();
+    /* Parse header from wire */
+    Header( const std::string & str );
 
-  /* A datagram was sent */
-  void datagram_was_sent( const uint64_t sequence_number,
-			  const uint64_t send_timestamp,
-			  const bool after_timeout );
+    /* Make wire representation of header */
+    std::string to_string() const;
+  } header;
 
-  /* An ack was received */
-  void ack_received( const uint64_t sequence_number_acked,
-		     const uint64_t send_timestamp_acked,
-		     const uint64_t recv_timestamp_acked,
-		     const uint64_t timestamp_ack_received );
+  std::string payload;
 
-  /* How long to wait (in milliseconds) if there are no acks
-     before sending one more datagram */
-  unsigned int timeout_ms();
+  /* New message */
+  ContestMessage( const uint64_t s_sequence_number,
+		  const std::string & s_payload );
+
+  /* Parse incoming datagram from wire */
+  ContestMessage( const std::string & str );
+
+  /* Fill in the send_timestamp for an outgoing datagram */
+  void set_send_timestamp();
+
+  /* Make wire representation of datagram */
+  std::string to_string() const;
+
+  /* Transform into an ack of the ContestMessage */
+  void transform_into_ack( const uint64_t sequence_number,
+			   const uint64_t recv_timestamp );
+
+  /* Is this message an ack? */
+  bool is_ack() const;
 };
 
-#endif
+#endif /* CONTEST_MESSAGE_HH */
