341917e8dc344004aa3b1a5619bc48382abcdc2c
sam <sbedassa67@gmail.com>
2021-05-04 18:10:27 +0200

    second try

--- project3/scripts/src.py
+++ project3/scripts/src.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import random
+
 sat_positions = [[]]*40
 for i in range(40):
     sat_positions[i] = []
@@ -14,8 +18,41 @@ with open('../input_data/sat_positions.txt') as sats:
             "lat" : float( entry[3]), "lon": float(entry[4]), "alt" : float(entry[5][0:-2])}
             sat_positions[i].append(dic)
 
+vals_lis = []
+
+with open('../input_data/valid_isls.txt') as vals:
+    for i in range(340960):
+        line = vals.readline()
+        entry = line.split(',')
+        vals_lis.append(entry[:2])
+
+print(vals_lis[33])
+sats = [0]*1601
+rang = 340960
+connected = 0
+with open('../output_data/sat_links.txt','w') as out:
+    while connected < 1500:
+        i = random.randrange(0,rang)
+        c = vals_lis[i]
+        if sats[int(c[0])] < 4 and sats[int(c[1])] < 4:
+            sats[int(c[0])]+=1
+            sats[int(c[1])]+=1
+            if sats[int(c[0])]==4:
+                connected+=1
+            if sats[int(c[1])]==4:
+                connected+=1
+            outt = str(c[0]) + ',' + str(c[1]) + '\n' 
+            out.writelines(outt)
+        rang-=1
+        vals_lis.remove(c)
+        print(connected, len(vals_lis),i)
+
+    
+
+
 links = []
 
+"""
 with open('../output_data/sat_links.txt','w') as out:
     for i in range(40):
         for j in range(40):
@@ -31,3 +68,4 @@ with open('../output_data/sat_links.txt','w') as out:
             out.writelines(out1)
             out.writelines(out2)
 
+"""
